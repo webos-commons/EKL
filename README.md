b221440b52980c908f18ea4400a28f78126642d1
Daniel Cousineau <dcousineau@gmail.com>
2011-05-23 18:00:17 -0500

    Added enyo libraries, added README.md, added mousewheel support, added initial work on upgraded VirtualList

---
+++ README.md
@@ -0,0 +1,16 @@
+EKL - Enyo Component Library
+============================
+
+A collection of Enyo kinds (components) as well as core Enyo extensions for use
+by other Enyo developers.
+
+
+
+License
+-------
+
+All components are licensed under the MIT license and may be freely used and
+distributed in any open source and commercial applications. If at all possible
+it is requested that you credit any components used.
+
+Enyo is currently under NDA.
---
+++ components/List/VirtualList.js
@@ -0,0 +1,24 @@
+enyo.kind({
+    name: "ekl.List.VirtualList",
+    kind: enyo.VirtualList,
+    
+    published: {
+        scrollbar: true,
+        mousescroll: true,
+        rowcount: null
+    },
+    events: {
+    },
+    
+    mousewheelHandler: function(inSender, inEvent) {
+        var pt = Math.round(this.$.scroller.$.scroll.y) - inEvent.delta.y;
+		if (pt == this.$.scroller.pageTop) {
+			return;
+		}
+		// page top drives all page rendering / discarding
+		this.$.scroller.pageTop = pt;
+		// add or remove pages from either end to satisfy display requirements
+		this.$.scroller.updatePages();
+    }
+    
+});
